d38390df02e06973a95512a2aa2c8b979996960e
Ozelot Vanilla <shuihenyun@qq.com>
2023-07-25 23:29:17 +0800

    Struct - Change news structure to be more flexible

---
+++ pages/news/[[news_id_param]].vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import NewsOverview from "./overview.vue"
+
+const route_info = useRoute()
+const { news_id_param } = route_info.params
+let news_id: string
+if (news_id_param instanceof Array) { news_id = news_id_param.join() }
+else { news_id = news_id_param ?? "" }
+</script>
+
+<style scoped>
+@import "~/assets/style/common.css";
+</style>
+
+<template>
+    <div class="website_container" v-if="news_id.trim().length == 0">
+        <NewsOverview />
+    </div>
+    <div class="website_container" v-else>
+
+    </div>
+</template>
